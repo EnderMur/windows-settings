fe1f929e7e9d99f5854e5bad5469fa9e3c64b314
EnderMur <slime9968@gmail.com>
2026-06-04 20:54:56 +0300

    Add PNG icon path entries

--- src/icons/mod.rs
+++ src/icons/mod.rs
@@ -4,3 +4,10 @@ pub const TELEMETRY: &str = "🛡";
 pub const MEMORY: &str = "🧠";
 pub const CLEANUP: &str = "🧹";
 pub const SETTINGS: &str = "⚙";
+
+pub const HOME_PNG: &str = "icons/home.png";
+pub const UWP_PNG: &str = "icons/uwp.png";
+pub const TELEMETRY_PNG: &str = "icons/telemetry.png";
+pub const MEMORY_PNG: &str = "icons/memory.png";
+pub const CLEANUP_PNG: &str = "icons/cleanup.png";
+pub const SETTINGS_PNG: &str = "icons/settings.png";
